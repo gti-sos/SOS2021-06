70d254dca4a8db233080a047547308a84fc5caef
Goncamper <goncamper@alum.us.es>
2021-03-22 18:06:53 +0000

    /info/digitalstreaming-stats

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get("/cool",(request,response)=>{
 	console.log("New request to /cool has arrived");
 });
 
+app.get("/info/digitalstreaming-stats",(request,response)=>{
+	response.send("<!DOCTYPE html> <html> <body> <h2>Digital media stats</h2> <table style=\"width:100%\"> <tr> <th>country</th> <th>year</th>  <th>hours-viewed</th> <th>avg-age</th> <th>avg-audience</th> </tr> <tr> <td>spain</td> <td>2020</td> <td>18 410 000 000</td> <td>21</td> <td>2 500 000</td> </tr> <tr> <td>spain</td> <td>2019</td> <td>11 000 000 000</td> <td>21</td> <td>1 200 000</td> </tr> <tr> <td>spain</td> <td>2020</td> <td>6 190 000 000</td> <td>30</td> <td>871 000</td> </tr> <tr> <td>spain</td> <td>2019</td> <td>3 190 000 000</td> <td>30</td> <td>443 256</td> </tr> <tr> <td>spain</td> <td>2020</td> <td>3 100 000 000</td> <td>26</td> <td>408 000</td> </tr> <tr> <td>spain</td> <td>2019</td> <td>1 090 000 000</td> <td>26</td> <td>136 000</td> </tr> </table> </body></html>");
+});
+
 app.listen(port,() => {
 	console.log("Server ready listening on port" + port)
 });
