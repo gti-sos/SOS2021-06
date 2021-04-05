6fbe1d1ce148b48303d7c4799610e8a5b26148d8
Goncamper <goncamper@alum.us.es>
2021-04-05 20:04:34 +0000

    l06

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.get("/cool",(request,response)=>{
 	console.log("New request to /cool has arrived");
 });
 
-app.get("/info/digitalstreaming-stats",(request,response)=>{
-	response.send("<!DOCTYPE html> <html> <body> <h2>Digital media stats</h2> <table style=\"width:100%\"> <tr> <th>country</th> <th>year</th>  <th>hours-viewed</th> <th>avg-age</th> <th>avg-audience</th> </tr> <tr> <td>Twitch</td> <td>spain</td> <td>2020</td> <td>18 410 000 000</td> <td>21</td> <td>2 500 000</td> </tr> <tr> <td>Twitch</td> <td>spain</td> <td>2019</td> <td>11 000 000 000</td> <td>21</td> <td>1 200 000</td> </tr> <tr> <td>YouTube</td> <td>spain</td> <td>2020</td> <td>6 190 000 000</td> <td>30</td> <td>871 000</td> </tr> <tr> <td>YouTube</td> <td>spain</td> <td>2019</td> <td>3 190 000 000</td> <td>30</td> <td>443 256</td> </tr> <tr> <td>Facebook</td> <td>spain</td> <td>2020</td> <td>3 100 000 000</td> <td>26</td> <td>408 000</td> </tr> <tr> <td>Facebook</td> <td>spain</td> <td>2019</td> <td>1 090 000 000</td> <td>26</td> <td>136 000</td> </tr> </table> </body></html>");
-});
-
 app.get("/info/television-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Television Stats</h2> <table style=\"width:60%\"> <tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table><br></br><table style=\"width:60%\"><tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table> </body> </html>");
 });
@@ -28,6 +24,12 @@ app.get("/info/television-stats",(request,response)=>{
 app.get("/info/onlinemedia-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Payment Platform Stats</h2> <table style=\"width:100%\"> <tr>  <th>name</th> <th>country</th> <th>year</th> <th>account price</th> <th>GPS mark</th> <th>subs</th> </tr> <tr> <td>Netflix</td> <td>Spain</td> <td>2020</td> <td>7,99€/month</td> <td>4,5</td> <td>4,2M</td> </tr> <tr> <td>HBO</td> <td>Spain</td> <td>2020</td> <td>8,99€/month</td> <td>2,5</td> <td>731k</td> </tr> <tr>  <td>Amazon Prime</td> <td>Spain</td> <td>2020</td> <td>3,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr> <td>Disney Plus</td> <td>Spain</td>  <td>2020</td> <td>6,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr>  <td>Rakuten</td> <td>Spain</td> <td>2020</td> <td>6,99€/month</td> <td>4,0</td> <td>2,2M</td> </tr> </table> </body> </html>");
 	console.log("New request to /cool has arrived");
+});
+
+
+// API Streaming stats
+app.get("/info/digitalstreaming-stats",(request,response)=>{
+	response.send("<!DOCTYPE html> <html> <body> <h2>Digital media stats</h2> <table style=\"width:100%\"> <tr> <th>country</th> <th>year</th>  <th>hours-viewed</th> <th>avg-age</th> <th>avg-audience</th> </tr> <tr> <td>Twitch</td> <td>spain</td> <td>2020</td> <td>18 410 000 000</td> <td>21</td> <td>2 500 000</td> </tr> <tr> <td>Twitch</td> <td>spain</td> <td>2019</td> <td>11 000 000 000</td> <td>21</td> <td>1 200 000</td> </tr> <tr> <td>YouTube</td> <td>spain</td> <td>2020</td> <td>6 190 000 000</td> <td>30</td> <td>871 000</td> </tr> <tr> <td>YouTube</td> <td>spain</td> <td>2019</td> <td>3 190 000 000</td> <td>30</td> <td>443 256</td> </tr> <tr> <td>Facebook</td> <td>spain</td> <td>2020</td> <td>3 100 000 000</td> <td>26</td> <td>408 000</td> </tr> <tr> <td>Facebook</td> <td>spain</td> <td>2019</td> <td>1 090 000 000</td> <td>26</td> <td>136 000</td> </tr> </table> </body></html>");
 });
 
 var streaming = [
@@ -81,10 +83,109 @@ var streaming = [
 	}
 	
 ];
-	
+
+
+var strstats = [];
+
+
+
+//5,2
+app.get(BASE_API_PATH +"/streaming-stats/loadInitialData", (req,res)=>{ 
+	//res.send(JSON.stringify(streaming,null,2));
+	if (strstats.length == 0){
+		for (var i=0;i <strstats.length;i++){
+			strstats.push(streaming[i]);
+		}
+		console.log('Datos cargados correctamente')
+		return res.sendStatus(200).json(strstats);
+	}
+});
+//6.1
 app.get(BASE_API_PATH +"/streaming", (req,res)=>{ 
 	res.send(JSON.stringify(streaming,null,2));
 });
+
+//6.2
+app.post(BASE_API_PATH +"/streaming-stats", (req,res)=>{ 
+	var newStat = req.body;
+	console.log(`new stat to be added: <${JSON.stringify(newStat,null,2)}>`);
+	streaming.push(newStat);
+	res.sendStatus(201);
+});
+
+//6.3
+app.get(BASE_API_PATH +"/streaming-stats/:country/:year", (req,res)=>{
+	var pais = req.params.country;
+	var ano = req.params.year;
+	
+	for (var stat of streaming){
+		if (stat.country == pais && stat.ano == year){
+			return res.sendStatus(200).json(stat);
+		}
+		else {
+			res.sendStatus(404);
+		}
+	}
+});
+
+//6.4
+app.delete(BASE_API_PATH+"/streaming-stats/:country/:year", (req,res)=>{
+	var pais = req.params.country;
+	var ano = req.params.year;
+	
+	if (streaming.length != 0){
+		for (var i=0; i<streaming.length;i++){
+			if(streaming[i].country == pais && streaming[i].year == ano){
+				streaming.splice(i,1);
+				return res.sendStatus(200);
+			}
+		}
+	}
+	return res.sendStatus(404);
+});
+
+//6.5
+app.put(BASE_API_PATH+"/streaming-stats/:country/:year", (req,res)=>{
+	var pais = req.params.country;
+	var ano = req.params.year;
+	var newStat = req.body;
+	
+	for(var i = 0; i<streaming.length;i++){
+		streaming[i]["country"] = pais;
+		streaming[i]["year"] = ano;
+		streaming[i]["hours-viewed"] = newStat['hours-viewed'];
+		streaming[i]["avg-age"] = newStat['avg-age'];
+		streaming[i]["avg-audience"] = newStat['avg-audience'];
+		res.sendStatus(200);
+	}
+	
+});
+
+//6.6
+
+app.post(BASE_API_PATH+"streaming-stats/:country/:year", (req, res) => {
+	console.log("Metodo no permitido");
+	res.sendStatus(405);
+});
+
+//6.7
+app.put(BASE_API_PATH + "/streaming-stats", (req,res)=>{
+	console.log("Metodo no permitido");
+	res.sendStatus(405);
+});
+
+//6.8
+app.delete(BASE_API_PATH+"/streaming-stats", (req,res)=>{
+	if (streaming.length == 0){
+		console.log("No hay datos para borrar");
+		res.sendStatus(405);
+	} else {
+		streaming.length = 0;
+		console.log("Datos borrados correctamente");
+		res.sendStatus(200);
+	}
+});
+/////////////////////////////////////
 
 app.listen(port,() => {
 	console.log("Server ready listening on port" + port)
