29765cc2d5f307c5bcdff958d7b4291f081c2413
Daniel <dangalcar@alum.us.es>
2021-05-02 13:35:25 +0200

    create infoPage

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -7,12 +7,14 @@
 	import TelevisionApi from "./Television.svelte";
 	import OnlineMediaApi from "./OnlineMedia.svelte";
 	import StreamingAPI from "./Streaming.svelte";
+	import InfoPage from "./infoPage.svelte";
 	
 	const routes = {
 		"/":Home,
 		"/television-stats": TelevisionApi,
 		"/onlinemedia-stats": OnlineMediaApi,
 		"/streaming-stats": StreamingAPI,
+		"/info": InfoPage,
 		'*': NotFound};
 </script>
 
---
+++ src/front/infoPage.svelte
@@ -0,0 +1,52 @@
+<div class="container">
+
+    <h4>Repositorio github SOS2021-06:</h4>
+    <p><a href="https://github.com/gti-sos/SOS2021-06">https://github.com/gti-sos/SOS2021-06</a>)</p>
+
+    <h4>Enlace a la pagina en Heroku</h4>
+    <p><a href="https://sos2021-06.herokuapp.com">https://sos2021-06.herokuapp.com</a></p>
+
+    <h4>APIs (Front-End)</h4>
+    <ul>
+        <li>Enlace a la API: <a href="#/television-stats">/television-stats</a>
+            (Realizado por: Daniel Gallardo Carrion)</li>
+        <li>Enlace a la API: <a href="#/onlinemedia-stats">/onlinemedia-stats</a>
+            (Realizado por: Salvador Romero Merino)</li>
+        <li>Enlace a la API: <a href="#/streaming-stats">/streaming-stats</a>
+            (Realizado por: Gonzalo Camacho Perez de Sevilla)</li>
+    </ul>
+
+    <h4>APIs (Back-End)</h4>
+    <ul>
+        <li>Enlace a la API: <a
+                href="/api/v1/television-stats">/television-stats</a>
+            (Realizado por: Daniel Gallardo Carrion) </li>
+        <li>Enlace a la API: <a
+                href="/api/v1/renewablepowercapacities-stats">/onlinemedia-stats</a>
+            (Realizado por: Salvador Romero Merino</li>
+        <li>Enlace a la API: <a
+                href="/api/v1/streaming-stats">/streaming-stats</a>
+            (Realizado por: Gonzalo Camacho Perez de Sevilla</li>
+    </ul>
+
+    
+
+    <h4>Integrantes del equipo:</h4>
+    <ul>
+        <li>Enlace a github de Daniel Gallardo Carrion (television-stats): <a
+                href="https://github.com/dangalcar">https://github.com/dangalcar</a><br>
+                Documentación Postman: <a href="https://documenter.getpostman.com/view/9776351/TzJoELMw"> Aquí</a></li>
+        <li>Enlace a github de Salvador Romero Merino (onlinemedia-stats): <a
+                href="https://github.com/SalvadorRomMer">https://github.com/SalvadorRomMer</a><br>
+                Documentación Postman: <a href="https://documenter.getpostman.com/view/9683549/TzJoE1Lc"> Aquí</a></li>
+        <li>Enlace a github de Gonzalo Camacho Perez de Sevilla (streaming-stats): <a
+                href="https://github.com/Goncamper">https://github.com/Goncamper</a><br>
+                Documentación Postman: <a href="https://documenter.getpostman.com/view/14967656/TzJoE1Bg"> Aquí</a></li>
+    </ul>
+</div>
+<style>
+    .container{
+        margin-top: 4em;
+        margin-bottom: 4.6em;
+    }
+</style>
