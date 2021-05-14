ae5922d2c0658494ed1db63a12483d3f0f33a017
goncamper <goncamper@alum.us.es>
2021-05-14 12:43:50 +0200

    indexGraph

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -6,8 +6,9 @@
 	import TelevisionApi from "./GUI Television/Television.svelte";
 	import TelevisionEdit from "./GUI Television/TelevisionEdit.svelte";
 	import OnlineMediaApi from "./OnlineMedia.svelte";
-	import StreamingAPI from "./Streaming.svelte";
-	import StreamEdit from "./StreamEdit.svelte";
+	import StreamingAPI from "./GUI Streaming/Streaming.svelte";
+	import StreamEdit from "./GUI Streaming/StreamEdit.svelte";
+	import StreamGraph from "./GUI Streaming/StreamGraph.svelte";
 	import InfoPage from "./infoPage.svelte";
 
 	
@@ -18,6 +19,7 @@
 		"/streaming-stats": StreamingAPI,
 		"/television-stats/:groupTV/:year":TelevisionEdit,
 		"/streaming-stats/:streamPlatform/:streamYear":StreamEdit,
+		"/streaming-stats/graph":StreamGraph,
 		"/info": InfoPage,
 		'*': NotFound
 		};
