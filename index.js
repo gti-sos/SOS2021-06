841bf6f05a23763b27d9780b4b4f93b68a960843
Daniel <dangalcar@alum.us.es>
2021-05-14 15:05:37 +0200

    error solution

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var cool = require("cool-ascii-faces");
+
 var express = require("express");
+
 var bodyParser = require("body-parser");
+
 var app= express();
+
 var port = (process.env.PORT || 10000);
+
 var path = require("path");
+
 var BASE_API_PATH = "/api/v1";
 var BASE_API_PATH2 = "/api/v2";
+
 
 app.use("/",express.static(path.join(__dirname , "public")));
 app.use(bodyParser.json());
@@ -15,18 +22,15 @@ app.get("/cool",(request,response)=>{
 	console.log("New request to /cool has arrived");
 });
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// API Television stats
 app.get("/info/television-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Television Stats</h2> <table style=\"width:60%\"> <tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table><br></br><table style=\"width:60%\"><tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table> </body> </html>");
 });
 
-// API Onlinemedia stats
 app.get("/info/onlinemedia-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Payment Platform Stats</h2> <table style=\"width:100%\"> <tr>  <th>name</th> <th>country</th> <th>year</th> <th>account price</th> <th>GPS mark</th> <th>subs</th> </tr> <tr> <td>Netflix</td> <td>Spain</td> <td>2020</td> <td>7,99€/month</td> <td>4,5</td> <td>4,2M</td> </tr> <tr> <td>HBO</td> <td>Spain</td> <td>2020</td> <td>8,99€/month</td> <td>2,5</td> <td>731k</td> </tr> <tr>  <td>Amazon Prime</td> <td>Spain</td> <td>2020</td> <td>3,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr> <td>Disney Plus</td> <td>Spain</td>  <td>2020</td> <td>6,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr>  <td>Rakuten</td> <td>Spain</td> <td>2020</td> <td>6,99€/month</td> <td>4,0</td> <td>2,2M</td> </tr> </table> </body> </html>");
-
+	
 });
+
 
 // API Streaming stats
 app.get("/info/digitalstreaming-stats",(request,response)=>{
@@ -38,15 +42,18 @@ app.get("/info/digitalstreaming-stats",(request,response)=>{
 var tv = require("./src/back/TelevisionAPI/indexTVv2.js");
 tv.register(app);
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var stream = require("./src/back/StreamingAPI/indexStream.js");
+
 stream.register(app);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var onlinemed = require("./src/back/OnlineMediaAPI/indexOnline.js");
+
 onlinemed.register(app);
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(port,() => {
 	console.log("Server ready listening on port: " + port)
