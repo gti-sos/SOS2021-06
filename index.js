e5743cfd973a217f00e3bff6116ab97c04207f2b
Daniel <dangalcar@alum.us.es>
2021-05-14 14:27:31 +0200

    edit navbar and create new svelte

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ app.get("/cool",(request,response)=>{
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// API Streaming stats
+
+// API Television stats
 app.get("/info/television-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Television Stats</h2> <table style=\"width:60%\"> <tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table><br></br><table style=\"width:60%\"><tr> <th>Group TV</th><th>country</th> <th>year</th> <th>cable/tv broadcast avg-audience-year</th> <th>Avg age</th> <th>avg-audience-month</th> </tr> <tr> <td>Telecinco</td> <td>Spain</td> <td>2019</td> <td align='center'>18.200.000</td> <td align='center'>58</td> <td align='center'>2.164.000</td> </tr> <tr> <td>Antena 3</td> <td>Spain</td> <td>2019</td> <td align='center'>19.800.000</td> <td align='center'>57</td> <td align='center'>2.330.000</td> </tr> <tr>  <td>La 1</td> <td>Spain</td> <td>2019</td> <td align='center'>10.500.000</td> <td align='center'>60</td> <td align='center'>1.725.000</td> </tr> </table> </body> </html>");
 });
 
-// API Streaming stats
+// API Onlinemedia stats
 app.get("/info/onlinemedia-stats",(request,response)=>{
 	response.send("<!DOCTYPE html> <html> <body> <h2>Payment Platform Stats</h2> <table style=\"width:100%\"> <tr>  <th>name</th> <th>country</th> <th>year</th> <th>account price</th> <th>GPS mark</th> <th>subs</th> </tr> <tr> <td>Netflix</td> <td>Spain</td> <td>2020</td> <td>7,99€/month</td> <td>4,5</td> <td>4,2M</td> </tr> <tr> <td>HBO</td> <td>Spain</td> <td>2020</td> <td>8,99€/month</td> <td>2,5</td> <td>731k</td> </tr> <tr>  <td>Amazon Prime</td> <td>Spain</td> <td>2020</td> <td>3,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr> <td>Disney Plus</td> <td>Spain</td>  <td>2020</td> <td>6,99€/month</td> <td>4,4</td> <td>1,2M</td> </tr> <tr>  <td>Rakuten</td> <td>Spain</td> <td>2020</td> <td>6,99€/month</td> <td>4,0</td> <td>2,2M</td> </tr> </table> </body> </html>");
 
-})
+});
 
 // API Streaming stats
 app.get("/info/digitalstreaming-stats",(request,response)=>{
