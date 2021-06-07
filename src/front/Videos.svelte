6f2ebc2f4e6997a5b7b744ad79a4b3a0a4125d41
Daniel <dangalcar@alum.us.es>
2021-06-07 19:35:37 +0200

    pag videos

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -1,10 +1,10 @@
 
 <main>
-
+    <h1>Enlaces a los videos del equipo</h1><br>
     <div class="card-group">
         
-        <div class="card border-success mb-6" style="">
-                <div class="card-body text-success">
+        <div class="card border-dark mb-6" style="">
+                <div class="card-body text-info">
                   <div class="embed-responsive embed-responsive-16by9 video">
                     <iframe width="816" height="441" src="https://www.youtube.com/embed/ECpxq47U6eU" frameborder="0"  allowfullscreen></iframe>
                 </div>
@@ -18,24 +18,24 @@
         </div>
         
         
-        <div class="card border-success mb-6" style="">
-            <div class="card-body text-success">
+        <div class="card border-dark mb-6" style="">
+            <div class="card-body text-info">
               <div class="embed-responsive embed-responsive-16by9 video">
-                <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+                <iframe width="816" height="441" src="https://www.youtube.com/embed/SxrJCP6GgUg" frameborder="0"  allowfullscreen></iframe>
             </div>
-              <h5 class="card-title" style="padding-left: 28%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Daniel Gallardo Carrión</a></h5>
-              <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
-                  en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+              <h5 class="card-title" style="padding-left: 28%; margin-top: 3%;"><a href="https://github.com/dangalcar">Daniel Gallardo Carrión</a></h5>
+              <p class="card-text">Video explicativo de los distintos aspectos y funcionalidades de la pagina del proyecto, y el apartado de datos con respecto 
+                  a los grupos de television.
               </p>
             </div>
             <div class="card-footer bg-transparent border-success">
-                <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+                <a href="https://youtu.be/SxrJCP6GgUg" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
             </div>
     
         </div>
 
-        <div class="card border-success mb-6" style="">
-            <div class="card-body text-success">
+        <div class="card border-dark mb-6" style="">
+            <div class="card-body text-info">
               <div class="embed-responsive embed-responsive-16by9 video">
                 <iframe width="816" height="441" src="https://www.youtube.com/embed/TchuF-7i-Es" frameborder="0"  allowfullscreen></iframe>
             </div>
@@ -60,5 +60,11 @@
     }
     .card-group{
         margin-top: auto;
+    }
+    h1{
+        font-size: 250%;
+        text-align: center;
+        margin-top: 2%;
+        text-decoration: underline;
     }
 </style>
