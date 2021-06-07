b6d41498075001ea0f92dc766b927ca007768465
Daniel <dangalcar@alum.us.es>
2021-06-07 18:54:10 +0200

    pag videos

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -1,7 +1,66 @@
-<script> 
 
-</script>
+<main>
 
-<main> 
+    <div class="card-group">
+        
+        <div class="card border-success mb-6" style="">
+                <div class="card-body text-success">
+                  <div class="embed-responsive embed-responsive-16by9 video">
+                    <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+                </div>
+                  <h5 class="card-title" style="padding-left: 18%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Gonzalo Camacho Pérez de Sevilla</a></h5>
+                  <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
+                      en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+                  </p>
+                </div>
+                <div class="card-footer bg-transparent border-success">
+                    <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+                </div>
+        </div>
+        
+        
+        <div class="card border-success mb-6" style="">
+            <div class="card-body text-success">
+              <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+            </div>
+              <h5 class="card-title" style="padding-left: 28%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Daniel Gallardo Carrión</a></h5>
+              <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
+                  en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+              </p>
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+            </div>
     
+        </div>
+
+        <div class="card border-success mb-6" style="">
+            <div class="card-body text-success">
+              <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+            </div>
+              <h5 class="card-title" style="padding-left: 30%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Salvador Romero Merino </a></h5>
+              <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
+                  en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+              </p>
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+            </div>
+        </div>   
+    </div>
 </main>
+
+<style>
+    .card{
+        max-height: 50rem;
+        max-width: 50rem;
+    }
+    .card.border-success.mb-6{
+        max-width: 40rem; 
+    }
+    .card-group{
+        margin-top: auto;
+    }
+</style>
