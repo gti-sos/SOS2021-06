7dba550358d5fab8bf2a0ba3399fa9bfee52e2d4
SalvadorRomMer <byrommer7@gmail.com>
2021-06-07 19:20:54 +0200

    D03

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -37,15 +37,14 @@
         <div class="card border-success mb-6" style="">
             <div class="card-body text-success">
               <div class="embed-responsive embed-responsive-16by9 video">
-                <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+                <iframe width="816" height="441" src="https://www.youtube.com/embed/TchuF-7i-Es" frameborder="0"  allowfullscreen></iframe>
             </div>
-              <h5 class="card-title" style="padding-left: 30%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Salvador Romero Merino </a></h5>
-              <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
-                  en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+              <h5 class="card-title" style="padding-left: 30%; margin-top: 3%;"><a href="https://github.com/SalvadorRomMer">Salvador Romero Merino </a></h5>
+              <p class="card-text"> Demo de presentación del proyecto de la asignatura SOS2021
               </p>
             </div>
             <div class="card-footer bg-transparent border-success">
-                <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+                <a href="https://youtu.be/TchuF-7i-Es" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
             </div>
         </div>   
     </div>
