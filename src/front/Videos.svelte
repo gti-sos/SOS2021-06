1fc57d4b8145ac9add321cba3aa0230bf7453be2
goncamper <goncamper@alum.us.es>
2021-06-07 19:09:11 +0200

    D03

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -6,15 +6,14 @@
         <div class="card border-success mb-6" style="">
                 <div class="card-body text-success">
                   <div class="embed-responsive embed-responsive-16by9 video">
-                    <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+                    <iframe width="816" height="441" src="https://www.youtube.com/embed/ECpxq47U6eU" frameborder="0"  allowfullscreen></iframe>
                 </div>
-                  <h5 class="card-title" style="padding-left: 18%; margin-top: 3%;"><a href="https://github.com/josemarimaya">Gonzalo Camacho Pérez de Sevilla</a></h5>
-                  <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
-                      en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
+                  <h5 class="card-title" style="padding-left: 18%; margin-top: 3%;"><a href="https://github.com/goncamper">Gonzalo Camacho Pérez de Sevilla</a></h5>
+                  <p class="card-text">Video de el entregable D03 de SOS de Gonzalo Camacho Pérez de Sevilla en el que se muestra funcionalidad final de las tablas y las graficas
                   </p>
                 </div>
                 <div class="card-footer bg-transparent border-success">
-                    <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
+                    <a href="https://youtu.be/ECpxq47U6eU" class="btn btn-info" style="margin-left: 35%;">Enlace al video</a>
                 </div>
         </div>
         
